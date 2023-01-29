5fe64becdb943956a792a00d6402e08fb439885a
WojciechTyczynski <wojty98@gmail.com>
2023-01-29 23:44:59 +0100

    Multiple files handling

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import utilities as ut
 from loguru import logger
 import pandas as pd
 import uvicorn
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -16,8 +17,20 @@ def health():
 
 # Define root endpoint
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    content = """
+<body>
+<form action="/transcribe/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
 @app.get('/greet', status_code=200)
 def say_hello():
@@ -25,17 +38,22 @@ def say_hello():
 
 # Define endpoint to transcribe a file
 @app.post("/transcribe/")
-def transcribe_file(audio_file: UploadFile):
-    logger.info(f"File loaded: {audio_file.filename}")
-    logger.info(f"Converting audio file...")
-    audio = ut.load_audio_file(audio_file.file)
-    logger.info(f"Audio file converted")
-    logger.info(f"Transcribing audio file...")
-    transcribtion = transcribe_api(api_config, audio)
-    df = pd.DataFrame(transcribtion)
-    results = df[['start', 'end', 'text']].to_dict('dict')
-    # results = ut.load_audio_file(audio_file.file)
-    return {"filename": audio_file.filename, "file_type": audio_file.content_type, "results": results}
+def transcribe_file(
+    audio_files: list[UploadFile] = File()
+):
+    response = {}
+    for audio_file in audio_files:
+        logger.info(f"File loaded: {audio_file.filename}")
+        logger.info(f"Converting audio file...")
+        audio = ut.load_audio_file(audio_file.file)
+        logger.info(f"Audio file converted")
+        logger.info(f"Transcribing audio file...")
+        transcribtion = transcribe_api(api_config, audio)
+        df = pd.DataFrame(transcribtion)
+        results = df[['start', 'end', 'text']].to_dict('dict')
+        # results = ut.load_audio_file(audio_file.file)
+        response[audio_file.filename] = results
+    return response
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
