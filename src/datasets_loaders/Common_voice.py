2d90ed6040514e58a3d19bdec6af42565628cb53
WojciechTyczynski <wojty98@gmail.com>
2023-02-13 15:48:07 +0100

    Common voice - da

--- src/datasets_loaders/Common_voice.py
+++ src/datasets_loaders/Common_voice.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 import torch
+import torchaudio
 import whisper
 from datasets import Audio
 
@@ -8,7 +9,7 @@ class Common_voice(torch.utils.data.Dataset):
     A simple class to wrap fleurs and trim/pad the audio to 30 seconds.
     """
     def __init__(self, split="test", device='cpu', language='da'):
-        self.dataset = load_dataset("mozilla-foundation/common_voice_11_0", language, split=split)
+        self.dataset = load_dataset("mozilla-foundation/common_voice_11_0", language, split=split, cache_dir='/work3/s212373/common_voice')
         self.dataset =  self.dataset.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
         self.data = self.dataset.cast_column("audio", Audio(sampling_rate=16000))
         self.device = device
@@ -20,7 +21,10 @@ class Common_voice(torch.utils.data.Dataset):
         audio =  torch.from_numpy(self.dataset[item]['audio']['array'])
         text = self.dataset[item]['sentence']
         sample_rate = self.dataset[item]['audio']['sampling_rate']
-        assert sample_rate == 16000
+        try:
+            assert sample_rate == 16000
+        except:
+            audio = torchaudio.functional.resample(audio, sample_rate, 16000)
         audio = whisper.pad_or_trim(audio.float()).to(self.device)
         mel = whisper.log_mel_spectrogram(audio)
         
