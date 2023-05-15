86ae8d590d9feef6c765a87fbf2926758152f338
mailtilmb@gmail.com <mailtilmb@gmail.com>
2023-05-15 16:53:39 +0200

    more clean up

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import torch
 import whisper
 from loguru import logger
-from transcribe import transcribe
 import benchmark as bm
 
 @torch.inference_mode()
@@ -33,23 +32,7 @@ def run(cfg) -> None :
     else:
         logger.error("Benchmark type not supported")
 
-    # check input
-    # inputs = ut.input_files_list(cfg.input)
-
-    # results = transcribe(cfg, inputs)
-
-    # # check if direrctory with transcriptions exists and create it if not
-    # if not os.path.exists(cfg.output):
-    #     os.mkdir(cfg.output)
-
-    # for transcribed_file in results:            
-        
-    #     # print(file)
-    #     # print(cfg.output)
-    #     print(f'{cfg.output}/{transcribed_file}_transcribtion.csv')
-    #     df = pd.DataFrame(results[transcribed_file])
-    #     df[['start', 'end', 'text']].to_csv(f'{cfg.output}/{transcribed_file}_transcribtion.csv', index=True)
-
 
 if __name__ == "__main__":
+    print("Running main.py")
     run()
