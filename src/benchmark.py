beface486351d1965aa7ebb97ddb3f746f2adeec
mailtilmb@gmail.com <mailtilmb@gmail.com>
2023-06-09 10:03:04 +0200

    batch file

--- src/benchmark.py
+++ src/benchmark.py
@@ -19,7 +19,7 @@ from huggingface_hub import login
 import json
 from transformers import WhisperProcessor, WhisperForConditionalGeneration, AutoProcessor, WhisperTokenizer, pipeline
 
-
+os.environ['TRANSFORMERS_CACHE'] = "/work3/s183954/transformers_cache"
 from datasets_loaders.Common_voice import Common_voice
 from datasets_loaders.Common_voice_5_1 import Common_voice_5_1
 from datasets_loaders.Common_voice_11 import Common_voice_11
@@ -349,7 +349,7 @@ def benchmark_longform_wer(cfg, options:whisper.DecodingOptions):
         hypotheses = []
         references = []
         language_token = f"<|{cfg.benchmark.language}|>"
-        if cfg.model.split('.')[1] == 'en':
+        if "en" in cfg.model:
             generate_kwargs = {"task":"transcribe", "no_repeat_ngram_size":5}
         else:
             generate_kwargs = {"task":"transcribe", "language":language_token, "no_repeat_ngram_size":5}
@@ -357,11 +357,11 @@ def benchmark_longform_wer(cfg, options:whisper.DecodingOptions):
 
         for audio, text in tqdm(loader):
             if cfg.batch_size == None:
-                results = pipe(audio, chunk_length_s=30,
+                results = pipe(audio, chunk_length_s=20,
                             generate_kwargs = generate_kwargs)
             else:    
                 results = pipe(
-                    audio, return_timestamps=True, chunk_length_s=30, stride_length_s=[6,0], batch_size=cfg.batch_size,
+                    audio, return_timestamps=True, chunk_length_s=30, stride_length_s=[6,3], batch_size=cfg.batch_size,
                     generate_kwargs = generate_kwargs
                 )
             hypotheses.extend([results['text']])
